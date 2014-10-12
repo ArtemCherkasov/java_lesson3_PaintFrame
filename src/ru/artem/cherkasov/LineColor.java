747f4b85e8ff9d46ac224b1512af20e409ca6d63
ArtemCherkasov <artemich_2001@mail.ru>
2014-10-12 15:08:37 +0400

    A.Cherkasov 12.10.2014

--- src/ru/artem/cherkasov/LineColor.java
+++ src/ru/artem/cherkasov/LineColor.java
@@ -13,16 +13,19 @@ public class LineColor {
 		return line2DFloat;
 		
 	}
+	
 	public void setLine2DFloat(Line2D.Float line2dFloat) {
 		
 		line2DFloat = line2dFloat;
 		
 	}
+	
 	public Color getColor() {
 		
 		return color;
 		
 	}
+	
 	public void setColor(Color color) {
 		
 		this.color = color;
